e1d695b7617210f032e69ab6f36bcd6ef3c6ee52
Jarosław Żak <jaroslaw.zak@microsoft.wsei.edu.pl>
2019-03-30 18:51:56 +0100

    Wersja jednoturowa

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,10 +10,34 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Witaj!");
+            /*Console.WriteLine("Witaj!");
             Console.Write("Podaj swoje imię: ");
             string x = Console.ReadLine();
-            Console.WriteLine($"Witaj, {x}");
+            Console.WriteLine($"Witaj, {x}");*/
+
+            Random generator = new Random();
+            int generatedNumber = generator.Next(1, 101);
+
+            Console.WriteLine("Wylosowałem liczbę w zakresie 1-100.\nZgadnij ją!");
+
+#if (DEBUG)
+            Console.WriteLine("Wylosowana liczba: " + generatedNumber);
+#endif
+
+            Console.Write("Podaj swoją propozycję: ");
+
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Podałeś wartość: {answer}");
+
+            if (answer < generatedNumber)
+                Console.WriteLine("Za mało!");
+            else if (answer > generatedNumber)
+                Console.WriteLine("Za dużo!");
+            else
+                Console.WriteLine("Trafiono!");
+
+            Console.WriteLine("Koniec gry");
         }
     }
 }
